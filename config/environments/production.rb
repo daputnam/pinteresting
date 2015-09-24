c74b2ba01b6da9e6eda7e1195bb52d82ea5d4640
Dan Putnam <daniel.a.putnam@gmail.com>
2015-09-23 23:10:18 -0500

    added s3 host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,10 @@ Rails.application.configure do
   #TSets paperclip to upload images to amazon s3 
   config.paperclip_defaults = {
   :storage => :s3,
+  :s3_host_name => 's3-external-1.amazonaws.com',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
+    #:region => ENV['AWS_REGION'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
